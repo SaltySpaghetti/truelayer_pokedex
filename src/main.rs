f973e23a7b1f6dc889735392fb04af4096dfc0c8
Salty Spaghetti <matteoterzuolo@gmail.com>
2026-06-16 14:40:13 +0200

    refactor: moved search_pokemon logic to a separate function to reuse it for translation endpoint + removed app state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod models;
+mod utils;
 
 use axum::{
     Json, Router,
-    extract::{Path, State},
+    extract::Path,
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
@@ -14,24 +15,16 @@ pub const POKE_API_BASE_URL: &str = "https://pokeapi.co/api/v2/pokemon-species";
 
 #[tokio::main]
 async fn main() {
-    let http_client = reqwest::Client::new();
-    let app = Router::new()
-        .route("/pokemon/{name}", get(search_pokemon))
-        .with_state(http_client);
+    let app = Router::new().route("/pokemon/{name}", get(search_pokemon_handler));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn search_pokemon(
-    State(http_client): State<reqwest::Client>,
-    Path(name): Path<String>,
-) -> Result<Json<PokemonDTO>, Response> {
+async fn search_pokemon(name: String) -> Result<PokemonResponse, Response> {
     let url = format!("{POKE_API_BASE_URL}/{name}");
 
-    let response = http_client
-        .get(url)
-        .send()
+    let response = reqwest::get(url)
         .await
         .and_then(reqwest::Response::error_for_status)
         .map_err(|err| reqwest_err(&err))?
@@ -39,7 +32,12 @@ async fn search_pokemon(
         .await
         .map_err(|err| reqwest_err(&err))?;
 
-    Ok(Json(PokemonDTO::from(response)))
+    Ok(response)
+}
+
+async fn search_pokemon_handler(Path(name): Path<String>) -> Result<Json<PokemonDTO>, Response> {
+    let pokemon = search_pokemon(name).await?;
+    Ok(Json(PokemonDTO::from(pokemon)))
 }
 
 fn reqwest_err(e: &reqwest::Error) -> Response {
